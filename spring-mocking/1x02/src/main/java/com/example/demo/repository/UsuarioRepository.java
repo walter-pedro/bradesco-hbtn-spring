b06fd1b5017310b4ba5a99e005decf1d74dc4aae
Walter Pedro <wal100lp@hotmail.com>
2025-11-03 16:03:23 +0000

    Adding abstract methods to UsuarioRepository class

--- spring-mocking/1x02/src/main/java/com/example/demo/repository/UsuarioRepository.java
+++ spring-mocking/1x02/src/main/java/com/example/demo/repository/UsuarioRepository.java
@@ -4,8 +4,13 @@ import com.example.demo.model.Usuario;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
 
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    public Optional<Usuario> findById(Long id);
+    public Usuario save(Usuario usuario);
 }
